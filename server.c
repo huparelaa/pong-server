24e443a84460770b039043b173266cddfaf1860c
Hobar <huparelaa@eafit.edu.co>
2023-09-23 09:20:19 -0500

    ♻️ Conecction client-server with UDP protocol

--- server.c
+++ server.c
@@ -1,139 +1,53 @@
-/*
-    C socket server example, handles multiple clients using threads
-    Compile
-    gcc server.c -lpthread -o server
-*/
- 
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<stdlib.h>    //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
-#include<pthread.h> //for threading , link with lpthread
- 
-//the thread function
-void *connection_handler(void *);
- 
-int main(int argc , char *argv[])
-{
-    int socket_desc , client_sock , c;
-    struct sockaddr_in server , client;
-     
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8080 );
-     
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
-    }
-    puts("bind done");
-     
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-     
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-	pthread_t thread_id;
-	
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-        puts("Connection accepted");
-         
-        if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
-        {
-            perror("could not create thread");
-            return 1;
-        }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( thread_id , NULL);
-        puts("Handler assigned");
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-        
+int main(int argc, char **argv){
 
-        
-    }
-     
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-     
-    return 0;
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
+
+  char *ip = "127.0.0.1";
+  int port = atoi(argv[1]);
+
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[1024];
+  socklen_t addr_size;
+  int n;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0){
+    perror("[-]socket error");
+    exit(1);
+  }
+
+  memset(&server_addr, '\0', sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  n = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (n < 0) {
+    perror("[-]bind error");
+    exit(1);
+  }
+
+  bzero(buffer, 1024);
+  addr_size = sizeof(client_addr);
+  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
+  printf("[+]Data recv: %s\n", buffer);
+
+  bzero(buffer, 1024);
+  strcpy(buffer, "Welcome to the UDP Server.");
+  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
+  printf("[+]Data send: %s\n", buffer);
+
+  return 0;
 }
- 
-/*
- * This will handle connection for each client
- * */
-void *connection_handler(void *socket_desc)
-{
-    //Get the socket descriptor
-    int sock = *(int*)socket_desc;
-    int read_size;
-    char *message , client_message[2000];
-    
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
-     
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
-     
-    //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-    {
-        //end of string marker
-		client_message[read_size] = '\0';
-		
-        //verificamos si es exit
-        if(strcmp(client_message, "exit") == 0){
-            printf("Cliente desconectado\n");
-            break;
-        }
-        //mostramos el mensaje del cliente junto con Ip y puerto
-        printf("Cliente: %s\n", client_message);
-        //Send the message back to client
-        write(sock , client_message , strlen(client_message));
-
-
-
-
-		//clear the message buffer
-		memset(client_message, 0, 2000);
-
-    }
-     
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-    // Client closed socket so clean up
-    close(sock);
-    return 0;
-} 
